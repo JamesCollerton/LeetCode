47394c2b9f5782dde54ca683253b445c92ea5b35
James Collerton <jamescollerton@live.co.uk>
2021-04-09 19:18:22 +0100

    Second attempt, still not quite right

--- HashMap-Isomorphic-Strings/HashMapIsomorphicStrings.scala
+++ HashMap-Isomorphic-Strings/HashMapIsomorphicStrings.scala
@@ -1,19 +1,22 @@
-import scala.collection.mutable.HashMap
+import scala.collection.immutable.List 
 
 object Solution {
     def isIsomorphic(s: String, t: String): Boolean = {
         makeStringCharCountArr(s) == makeStringCharCountArr(t)
     }
     
-    def makeStringCharCountArr(s: String): Set[Int] = {
-        val stringCharMap = s.foldLeft(Map.empty[Char,Int])((map, value) => {
-            if(!map.contains(value)){
-                map + (value -> 1)
+    def makeStringCharCountArr(s: String): List[Int] = {
+        val stringCharList = s.foldLeft(List[(Char,Int)]())((arr, value) => {
+            if(arr.length == 0) {
+                arr :+ (value, 1)
+            } else if(arr.last._1 == value) {
+                val lastCount = arr.last
+                val toAddArr = arr.dropRight(1)
+                toAddArr :+ (value, lastCount._2 + 1)
             } else {
-                map + (value -> (map(value) + 1))
+                arr :+ (value, 1)
             }
         })
-        println(stringCharMap.values.toSet)
-        stringCharMap.values.toSet
+        stringCharList.map(_._2)
     }
 }
