784268879eed2cd84ecbf11901a0b7768b4341d9
James Collerton <jamescollerton@live.co.uk>
2021-04-19 20:07:01 +0100

    Still not a right solution

--- HashMap-4sum/HashMap4Sum.java
+++ HashMap-4sum/HashMap4Sum.java
@@ -4,48 +4,71 @@ class Solution {
         return kSum(nums, target, 4);  
     }
     
-    private List<List<Integer>> kSum(int[] nums, int target, int k) {
-        
-        // Sort the list O(log(n))
-        
-        // We want to be k apart so [0], [0 + 1], [0 + 2], ... [0 + k]
-            
-        // We can have a recursive step, just by chopping off the first of the array         
-        
-        // Once we get over k we can finish, as the list is sorted we know the numbers 
-        // only get higher.
-        
-        // Make sure we check the list hasn't ended.
+    private List<List<Integer>> kSum(int[] nums, int target, int k) { 
         
         Arrays.sort(nums);
-        return kSumStep(nums, target, k);
+        
+        System.out.println("Sorted array: " + Arrays.toString(nums));
+        
+        int currIncr = k - 1;
+        
+        System.out.println("Current increment: " + currIncr);
+        
+        int[] pointerArr = new int[k];
+        for(int i = 0; i < k; i++) {
+            pointerArr[i] = i;
+        }
+        
+        System.out.println("Starting pointer array: " + Arrays.toString(pointerArr));
+        
+        return kSumStep(nums, target, k, currIncr, pointerArr);
     }
     
-    private List<List<Integer>> kSumStep(int[] nums, int target, int k) {
-        int sum = 0;
+    private List<List<Integer>> kSumStep(int[] nums, int target, int k, int currIncr, int[] pointerArr) {
+
         List<Integer> newSolutions = new ArrayList<Integer>();
-        for(int i = 0; i < k; i++) {
-            if(i < nums.length) {
-                sum += nums[i];
-                newSolutions.add(nums[i]);
-            } else {
-                // Past the end of the array, we're done.                 
+        
+        int sum = 0;
+        
+        System.out.println("");
+        System.out.println("In k Sum Step");
+        System.out.println("Current increment " + currIncr);
+        System.out.println("Pointer array " + Arrays.toString(pointerArr));
+        
+        // Iterate along pointers and take the sum
+        for(int i = 0; i < pointerArr.length; i++) {
+            int currPointer = pointerArr[i];             
+            if(currPointer < nums.length) {
+                sum += nums[currPointer];
+                newSolutions.add(nums[currPointer]);
+            // If the last pointer is falling off the edge then we should return
+            } else {                 
+                System.out.println("Quitting loop, current pointer " + currPointer);
                 return new ArrayList<List<Integer>>();
             }
         }
+                
+        System.out.println("Detected sum as " + sum);
         
-        if(sum > target) {
-            // We can stop now, as the array is sorted the total will only
-            // get higher
-            return new ArrayList<List<Integer>>();
-        }
+        // Move pointer along
+        pointerArr[currIncr] = pointerArr[currIncr] + 1;
+        
+        System.out.println("Moved pointer along, pointer array now " + Arrays.toString(pointerArr));
+        
+        int newIncr = (currIncr - 1 < 0) ? k - 1 : currIncr - 1;
+        
+        System.out.println("Change increment, now " + newIncr);
         
         if(sum == target) {
-            List<List<Integer>> arrayList = kSumStep(Arrays.copyOfRange(nums, 1, nums.length), target, k);
+            System.out.println("Found a solution! " + newSolutions);
+            
+            List<List<Integer>> arrayList = kSumStep(nums, target, k, newIncr, pointerArr);
             arrayList.add(newSolutions);
             return arrayList;
         } else {
-            List<List<Integer>> arrayList = kSumStep(Arrays.copyOfRange(nums, 1, nums.length), target, k);
+            System.out.println("Not a solution " + newSolutions);
+            
+            List<List<Integer>> arrayList = kSumStep(nums, target, k, newIncr, pointerArr);
             return arrayList;
         }
     }
