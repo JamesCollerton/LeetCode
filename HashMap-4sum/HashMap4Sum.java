7b016eddb2a22bbddfb0573443590002eaf150b4
James Collerton <jamescollerton@live.co.uk>
2021-04-19 19:41:16 +0100

    First wrong solution

--- HashMap-4sum/HashMap4Sum.java
+++ HashMap-4sum/HashMap4Sum.java
@@ -1,24 +1,52 @@
 class Solution {
         
     public List<List<Integer>> fourSum(int[] nums, int target) {
-
-        HashSet<List<Integer>> result = new HashSet<List<Integer>>();
+        return kSum(nums, target, 4);  
+    }
+    
+    private List<List<Integer>> kSum(int[] nums, int target, int k) {
         
-        for(int a = 0; a < nums.length; a++) {
-            for(int b = 0; b < nums.length; b++) {
-                for(int c = 0; c < nums.length; c++) {
-                    for(int d = 0; d < nums.length; d++) {
-                        if(Stream.of(a, b, c, d).distinct().count() == 4 &&
-                            nums[a] + nums[b] + nums[c] + nums[d] == target) {
-                            List<Integer> list = Arrays.asList(nums[a], nums[b], nums[c], nums[d]);
-                            Collections.sort(list);
-                            result.add(list);
-                        }
-                    }
-                }
+        // Sort the list O(log(n))
+        
+        // We want to be k apart so [0], [0 + 1], [0 + 2], ... [0 + k]
+            
+        // We can have a recursive step, just by chopping off the first of the array         
+        
+        // Once we get over k we can finish, as the list is sorted we know the numbers 
+        // only get higher.
+        
+        // Make sure we check the list hasn't ended.
+        
+        Arrays.sort(nums);
+        return kSumStep(nums, target, k);
+    }
+    
+    private List<List<Integer>> kSumStep(int[] nums, int target, int k) {
+        int sum = 0;
+        List<Integer> newSolutions = new ArrayList<Integer>();
+        for(int i = 0; i < k; i++) {
+            if(i < nums.length) {
+                sum += nums[i];
+                newSolutions.add(nums[i]);
+            } else {
+                // Past the end of the array, we're done.                 
+                return new ArrayList<List<Integer>>();
             }
         }
         
-        return new ArrayList<List<Integer>>(result);
+        if(sum > target) {
+            // We can stop now, as the array is sorted the total will only
+            // get higher
+            return new ArrayList<List<Integer>>();
+        }
+        
+        if(sum == target) {
+            List<List<Integer>> arrayList = kSumStep(Arrays.copyOfRange(nums, 1, nums.length), target, k);
+            arrayList.add(newSolutions);
+            return arrayList;
+        } else {
+            List<List<Integer>> arrayList = kSumStep(Arrays.copyOfRange(nums, 1, nums.length), target, k);
+            return arrayList;
+        }
     }
 }
