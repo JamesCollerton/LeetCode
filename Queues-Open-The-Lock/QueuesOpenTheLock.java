5fd1e1fef025bc8473072f3573fc4af448cac020
James Collerton <jamescollerton@live.co.uk>
2021-05-04 19:39:06 +0100

    Marginally optimised

--- Queues-Open-The-Lock/QueuesOpenTheLock.java
+++ Queues-Open-The-Lock/QueuesOpenTheLock.java
@@ -1,78 +1,82 @@
 class Solution {
     
+    ArrayList<String> seen = new ArrayList<String>();
+    
     public int openLock(String[] deadends, String target) {
                 
-        int[] initialSetting = new int[]{0, 0, 0, 0};
+        int level = 0;
+        List<Integer> initialSetting = List.of(0, 0, 0, 0);
         
-        return checkAllCombos(deadends, initialSetting, target, new ArrayList<String>());
+        Queue<List<Integer>> queue = new LinkedList();
+        queue.add(initialSetting);
+        
+        while(!queue.isEmpty()) {
+            
+            int size = queue.size();
+            
+            // Take all items off the queue at this level
+            while(size != 0){
+        
+                size -= 1;
+                
+                List<Integer> currentQueueEntry = queue.remove();
+                
+                String currentSettingString = convertSettingToString(currentQueueEntry);
 
-    }
-    
-    private int checkAllCombos(
-        String deadends[], 
-        int[] currentSetting, 
-        String target, 
-        List<String> seen
-    ) {
-        
-        String currentSettingString = convertSettingToString(currentSetting);
-        
-        // If we've already seen this then we know this route goes nowhere,
-        // or if we're at a deadend
-        if(deadendsContainsSetting(deadends, currentSettingString) || 
-           seen.contains(currentSettingString)){
-            return -1;
-        }
-        
-        seen.add(currentSettingString);
-        
-        // We've found the solution, we want to start counting up
-        if(target.equals(currentSettingString)) {
-            return 1;
-        // We want to create a tree of all possible moves
-        } else {
-            for(int i = 0; i < currentSetting.length; i++) {
-                
-                int[] oneStepForwardArray = takeStep(i, 1, currentSetting);
-                int oneStepForward = checkAllCombos(deadends, oneStepForwardArray, target, seen);
-                
-                // We've found a solution and we need to trace back
-                // up counting the steps
-                if(oneStepForward != -1) {
-                    return oneStepForward + 1;    
-                }
-                
-                int[] oneStepBackArray = takeStep(i, -1, currentSetting);
-                int oneStepBack = checkAllCombos(deadends, oneStepBackArray, target, seen);
-                
-                // We've found a solution and we need to trace back
-                // up counting the steps
-                if(oneStepBack != -1) {
-                    return oneStepBack + 1;    
+                // If we've already seen this then we know this route goes nowhere,
+                // or if we're at a deadend the same applies
+                if(!(
+                    deadendsContainsSetting(deadends, currentSettingString) || 
+                    seen.contains(currentSettingString)
+                )){
+
+                    seen.add(currentSettingString);
+
+                    // We've found the solution, we want to return the level
+                    // we're on
+                    if(target.equals(currentSettingString)) {
+                        return level; 
+                    } else {
+                        for(int i = 0; i < currentQueueEntry.size(); i++) {
+                            List<Integer> oneStepForward = takeStep(i, 1, currentQueueEntry); 
+                            queue.add(oneStepForward);
+                            
+                            // System.out.println("Here");
+
+                            List<Integer> oneStepBack = takeStep(i, -1, currentQueueEntry);
+                            queue.add(oneStepBack);
+                            
+                            // System.out.println("Here Again");
+                        }
+                    }
                 }
             }
             
-            return -1;
+            level++;
+            
         }
+        
+        return -1;
+
     }
     
-    private int[] takeStep(int position, int increment, int[] currentSetting) {
-        int[] newArray = Arrays.copyOf(currentSetting, currentSetting.length);
+    private List<Integer> takeStep(int position, int increment, List<Integer> currentSetting) {
+        List<Integer> newList = new ArrayList<>(currentSetting);
         
-        if(newArray[position] + increment == 10) {
-            newArray[position] = 0;
-        } else if(newArray[position] + increment == -1) {
-            newArray[position] = 9;
+        if(newList.get(position) + increment == 10) {
+            newList.set(position, 0);
+        } else if(newList.get(position) + increment == -1) {
+            newList.set(position, 9);
         } else {
-            newArray[position] += increment;
+            newList.set(position, newList.get(position) + increment);
         }
         
-        return newArray;
+        return newList;
     }
     
-    private String convertSettingToString(int[] currentSetting) {
-        return Arrays.stream(currentSetting)
-                        .mapToObj(String::valueOf)
+    private String convertSettingToString(List<Integer> currentSetting) {
+        return currentSetting.stream()
+                        .map(Object::toString)
                         .collect(Collectors.joining(""));
     }
     
