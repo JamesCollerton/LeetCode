0e692ef2d1b2a87e46fe9cf1a1bd7fdea1f1bc65
James Collerton <jamescollerton@live.co.uk>
2021-05-07 15:26:44 +0100

    Min heap 2

--- Heaps-Min-Heap/HeapsMinHeap.java
+++ Heaps-Min-Heap/HeapsMinHeap.java
@@ -101,7 +101,19 @@ public class Solution {
         }
         
         public void heapifyDown() {
-            
+            int index = 0;
+            while(hasLeftChild(index)) {
+                int smallerChildIndex = getLeftChildIndex(index);
+                if(hasRightChild(index) && rightChild(index) < leftChild(index)) {
+                    smallerChildIndex = getRightChildIndex(index);
+                }
+                if(items[index] < items[smallerChildIndex]) {
+                    break;
+                } else {
+                    swap(index, smallerChildIndex);
+                }
+                index = smallerChildIndex;
+            }    
         }
     }
 }
