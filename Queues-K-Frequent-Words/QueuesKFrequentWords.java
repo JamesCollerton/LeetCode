9406d6186bbb8b3f1eccc2ca73912210e74b8317
James Collerton <jamescollerton@live.co.uk>
2021-05-09 12:10:16 +0100

    Finished with stack

--- Queues-K-Frequent-Words/QueuesKFrequentWords.java
+++ Queues-K-Frequent-Words/QueuesKFrequentWords.java
@@ -2,17 +2,10 @@ class Solution {
     public List<String> topKFrequent(String[] words, int k) {
 
         HashMap<String, Integer> map = new HashMap<String, Integer>();
-        Queue<String> result = new LinkedList<>();
+        Stack<String> result = new Stack<>();
         
-        // O(n)
         for(String word: words) {
-            
-            System.out.println("");
-            System.out.println("Word " + word);
-            System.out.println("Current state of map");
-            map.forEach((key, value) -> System.out.println(key + ":" + value));
-            
-            // Increment the count in the map O(1)
+                        
             if(map.containsKey(word)) {
                 map.put(word, map.get(word) + 1);
             } else {
@@ -22,20 +15,11 @@ class Solution {
             // Look at the count of the current word
             int count = map.get(word);
             
-            System.out.println("Current word count " + count);
-            System.out.println("Finding place in queue");
-            
             findPlaceInQueue(map, result, word, count, k);
             
-            System.out.println("New queue");
-            result.forEach(System.out::println);
-        
         }
         
-        List<String> toReturn = (List) result;
-        Collections.reverse(toReturn);
-        
-        return toReturn;
+        return new ArrayList(result);
     }
 
     // We want to pop items off the queue as long
@@ -47,34 +31,28 @@ class Solution {
     // queue is less than size k
     private void findPlaceInQueue(
         HashMap<String, Integer> map, 
-        Queue<String> result, 
+        Stack<String> result, 
         String currentWord, 
         int currentCount,
         int k
     ) {
         
-        // If the queue is empty we can add the word as it
+        // If the stack is empty we can add the word as it
         // must have the highest frequency
         if(result.isEmpty()) {
-            System.out.println("Detected queue as empty, adding word '" + currentWord + "'");
             result.add(currentWord);
             return;
         }
         
-        // Otherwise get the last word from the queue and
+        // Otherwise get the last word from the stack and
         // find how many times we have seen it.
-        String poppedWord = result.remove();
+        String poppedWord = result.pop();
         int poppedWordCount = map.get(poppedWord);
-        
-        System.out.println("Last popped word '" + poppedWord + "'");
-        System.out.println("Last popped word count: " + poppedWordCount);
         
         // If we have seen the word coming in more often than the
         // one we have just popped off, then we need to keep popping
         // items off the queue to find a place
         if(currentCount > poppedWordCount) {
-            
-            System.out.println("Detected current count " + currentCount + " greater than " + poppedWordCount);
             
             // Keep popping items off queue to find the place
             findPlaceInQueue(map, result, currentWord, currentCount, k);
@@ -84,8 +62,7 @@ class Solution {
             // word back on the queue if it's equal to the current word
             // as then we get repeats.
             if(result.size() < k && !currentWord.equals(poppedWord)) {
-                System.out.println("Result size small, adding back popped word '" + poppedWord + "'");
-                result.add(poppedWord); 
+                result.push(poppedWord); 
             }
             
             return;
@@ -95,41 +72,28 @@ class Solution {
         } else if(currentCount == poppedWordCount) {
             
             // We need to sort these items
-            System.out.println("Detected current count " + currentCount + " equal to " + poppedWordCount);
             
             // Popped word is lower in the alphabet, therefore
             // needs to come before current word in queue
             if(poppedWord.compareTo(currentWord) < 1) {
-                
-                System.out.println("Detected popped word '" + poppedWord + "' before '" + currentWord + "''");
-                
+                                
                 // Bubble up the popped word, we want to add the 
                 findPlaceInQueue(map, result, poppedWord, poppedWordCount, k);
-                
-                System.out.println("Result size " + result.size());
-                
+                                
                 if(result.size() < k && !currentWord.equals(poppedWord)) {
-                    System.out.println("Result size less than "+ k + ", adding current word '" + currentWord + "'");
-                    result.add(currentWord); 
+                    result.push(currentWord); 
                 }
-                
-                System.out.println("Returning after adding");
-                
+                                
                 return;
                 
             } else if(poppedWord.compareTo(currentWord) == 0) {
                 throw new RuntimeException("Duplicate word in queue with same count");
             } else {
                 
-                System.out.println("Detected popped word '" + poppedWord + "' after '" + currentWord + "'");
-                
                 findPlaceInQueue(map, result, currentWord, currentCount, k);
                 
-                System.out.println("Looking to readd popped word '" + poppedWord + "' current word '" + currentWord + "'");
-                
                 if(result.size() < k && !currentWord.equals(poppedWord)) {
-                    System.out.println("Readding '" + poppedWord + "'");
-                    result.add(poppedWord); 
+                    result.push(poppedWord); 
                 } 
                 
                 return;
@@ -138,16 +102,13 @@ class Solution {
         } else if(currentCount < poppedWordCount) {
             // We can put this item on the queue as long
             // as the queue is less than k items long
-            System.out.println("Current count " + " less than popped word count " + poppedWordCount);
             
             if(result.size() < k) {
-                System.out.println("Adding back popped word '" + poppedWord + "'");
-                result.add(poppedWord); 
+                result.push(poppedWord); 
             }    
             
             if(result.size() < k && !currentWord.equals(poppedWord)) {
-                System.out.println("Adding back current word '" + poppedWord + "'");
-                result.add(currentWord); 
+                result.push(currentWord); 
             }
             
             return;
