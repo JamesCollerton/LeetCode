eed271bf5bcc05e7283cea7b74114c3e627f7ce1
James Collerton <jamescollerton@live.co.uk>
2021-09-14 18:27:57 +0100

    Works non-repeating

--- Binary-Search-Search-For-Range/BinarySearchForRange.java
+++ Binary-Search-Search-For-Range/BinarySearchForRange.java
@@ -1,39 +1,72 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-
-        int start = -1, end = -1;
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
         
-        if(nums == null || nums.length == 0) {
-            return new int[] {start, end};
+        List<Integer> result = new ArrayList<>();
+        
+        int nearestIndex = findNearestIndex(arr, x);
+        
+        while(nearestIndex - 1 >= 0 && arr[nearestIndex - 1] == arr[nearestIndex]) {
+            nearestIndex--;
         }
-
-        int left = 0, right = nums.length - 1;
         
-        while(left <= right){
-            // Prevent (left + right) overflow
+        System.out.println("Nearest index " + nearestIndex);
+        
+        result.add(arr[nearestIndex]);
+        
+        int numbersLeft = k -  1;
+        int left = nearestIndex - 1;
+        int right = nearestIndex + 1;
+        
+        while(numbersLeft > 0) {
+            if(left >= 0 && right < arr.length) {
+                int distanceOne = Math.abs(arr[left] - x);
+                int distanceTwo = Math.abs(arr[right] - x);
+                if(distanceOne <= distanceTwo) {
+                    result.add(0, arr[left]);
+                    left--;
+                } else {
+                    result.add(arr[right]);
+                    right++;
+                }
+            } else if (left >= 0) {
+                result.add(0, arr[left]);
+                left--;
+            } else {
+                result.add(arr[right]);
+                right++;
+            }
+            numbersLeft--;
+        }
+        
+        return result;
+    }
+    
+    private int findNearestIndex(int[] arr, int target) {
+        if(target < arr[0]) {
+            return 0;
+        }
+        
+        if(target > arr[arr.length - 1]) {
+            return arr.length - 1;
+        }
+        
+        int left = 0;
+        int right = arr.length - 1;
+        
+        while(left < right) {
+            
             int mid = left + (right - left) / 2;
             
-            if(nums[mid] == target){ 
-                start = mid;
-                end = mid;
-                while((start - 1) >= 0 && nums[start - 1] == target) {
-                    start--;
-                }
-                while((end + 1) < nums.length && nums[end + 1] == target) {
-                    end++;
-                }
-                return new int[] {start, end};
-            }
-            else if(nums[mid] < target) { 
-                left = mid + 1; 
-            }
-            else { 
-                right = mid - 1; 
-            }
+            int distanceOne = Math.abs(arr[mid] - target);
+            int distanceTwo = Math.abs(arr[mid + 1] - target);
+            
+            if(distanceTwo <= distanceOne) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            } 
         }
-
-        // End Condition: left > right
-        return new int[] {start, end};
         
+        return right;
     }
 }
