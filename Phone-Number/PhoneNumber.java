7f6e788fd84971870865e5dff9c2fdc485898f65
James Collerton <jamescollerton@live.co.uk>
2021-10-11 17:48:35 +0100

    Done phone number first part, now optimising

---
+++ Phone-Number/PhoneNumber.java
@@ -0,0 +1,47 @@
+class Solution {
+    
+    // Potentially replace this with fancy character maths
+    Map<Character, List<Character>> map = new HashMap<>();
+    
+    public List<String> letterCombinations(String digits) {
+        
+        // Create a map of the character to list of characters
+        map.put('2', Arrays.asList('a', 'b', 'c'));
+        map.put('3', Arrays.asList('d', 'e', 'f'));
+        map.put('4', Arrays.asList('g', 'h', 'i'));
+        map.put('5', Arrays.asList('j', 'k', 'l'));
+        map.put('6', Arrays.asList('m', 'n', 'o'));
+        map.put('7', Arrays.asList('p', 'q', 'r', 's'));
+        map.put('8', Arrays.asList('t', 'u', 'v'));
+        map.put('9', Arrays.asList('w', 'x', 'y', 'z'));
+                
+        // Return the list from the function
+        return recurse(new ArrayList<String>(), digits.toCharArray());
+    }
+    
+    private List<String> recurse(List<String> list, char[] digitArr) {
+        if(digitArr.length == 0) {
+            return list;
+        }
+        
+        List<Character> charList = map.get(digitArr[0]);
+        
+        List<String> newList = new ArrayList<>();
+        
+        if(list.isEmpty()) {
+            for(int i = 0; i < charList.size(); i++) {
+                newList.add(Character.toString(charList.get(i)));
+            }
+        } else {
+            for(int i = 0; i < charList.size(); i++) {
+                for(int j = 0; j < list.size(); j++) {
+                    // This could maybe be optimised
+                    newList.add(list.get(j) + charList.get(i));
+                }
+            }
+        }
+        
+        // Array copy could maybe be optimised using pointers      
+        return recurse(newList, Arrays.copyOfRange(digitArr, 1, digitArr.length));
+    } 
+}
