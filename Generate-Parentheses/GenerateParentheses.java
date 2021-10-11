875666eaae246fae8fbf3eb120a14d2ed4838bf0
James Collerton <jamescollerton@live.co.uk>
2021-10-11 19:13:14 +0100

    Backtracking parentheses

--- Generate-Parentheses/GenerateParentheses.java
+++ Generate-Parentheses/GenerateParentheses.java
@@ -1,47 +1,26 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        return recurse(n, 0, 0, new ArrayList<String>());
+        List<String> result = new ArrayList<String>();
+        recurse(n, 0, 0, result, "");
+        return result;
     }
     
-    private List<String> recurse(int n, int numOpen, int numClosed, List<String> list) {
+    private void recurse(int n, int numOpen, int numClosed, List<String> list, String str) {
         
         if(numOpen == n && numClosed == n) {
-            return list;
+            list.add(str);
+            return;
         }
         
         if(numOpen == numClosed) {
-            return addOpenBracket(n, numOpen, numClosed, list);
+            recurse(n, numOpen + 1, numClosed, list, str + "(");
         } else {
-            List<String> openBracketList = new ArrayList<>();
             if(numOpen < n) {
-                openBracketList = addOpenBracket(n, numOpen, numClosed, list);
+                recurse(n, numOpen + 1, numClosed, list, str + "(");
             }
-            List<String> closedBracketList = addClosedBracket(n, numOpen, numClosed, list);
-            openBracketList.addAll(closedBracketList);
-            return openBracketList;
+            recurse(n, numOpen, numClosed + 1, list, str + ")");
         }
         
     }
-    
-    private List<String> addOpenBracket(int n, int numOpen, int numClosed, List<String> list) {
-        List<String> newList = new ArrayList<>();
 
-        if(list.isEmpty()) {
-            newList.add("(");
-        } else {
-            for(int i = 0; i < list.size(); i++) {
-                newList.add(list.get(i) + "(");
-            }
-        }
-        return recurse(n, numOpen + 1, numClosed, newList);
-    }
-    
-    private List<String> addClosedBracket(int n, int numOpen, int numClosed, List<String> list) {
-        List<String> newList = new ArrayList<>();
-
-        for(int i = 0; i < list.size(); i++) {
-            newList.add(list.get(i) + ")");
-        }
-        return recurse(n, numOpen, numClosed + 1, newList);
-    }
 }
