7db099557242952b1cce7619fd29e972987d6906
James Collerton <jamescollerton@live.co.uk>
2021-10-19 18:07:24 +0100

    Did binary tree from preorder

--- BitMasking-Word-Product/BitMaskingWordProduct.java
+++ BitMasking-Word-Product/BitMaskingWordProduct.java
@@ -2,15 +2,27 @@ class Solution {
     public int maxProduct(String[] words) {
         int max = 0;
         
+        // Here we create a map of integers representing bit masks
+        // to the maximum length of a word with that bit mask.
         Map<Integer, Integer> maskToMaxLengthMap = new HashMap<>();
 
+        // Populating the map
         for(int i = 0; i < words.length; i++){
             
+            // This is the same as saying the mask is just a string of 
+            // zeros
             Integer mask = 0;
+
+            // For each character we find what letter of the alphabet it
+            // is (c - 'a'), then we shift our one along that number of 
+            // places. By using |= we are leaving any bits that are already
+            // in their place set, but also setting our new bit.
             for(char c: words[i].toCharArray()){
                 mask |= 1 << (c - 'a');
             }
             
+            // If the map already contains this mask then just put the
+            // longest string value in, we don't care about shorter ones.
             if(maskToMaxLengthMap.containsKey(mask)) {
                 if(maskToMaxLengthMap.get(mask) < words[i].length()) {
                     maskToMaxLengthMap.put(mask, words[i].length());
@@ -20,6 +32,7 @@ class Solution {
             }
         }
         
+        // Iterate through all of the masks and compare
         List<Integer> masks = maskToMaxLengthMap.keySet().stream().collect(Collectors.toList());
         
         for(int i = 0; i < masks.size(); i++) {
@@ -28,6 +41,9 @@ class Solution {
                 int maskOne = masks.get(i);
                 int maskTwo = masks.get(j);
                 
+                // If the bitwise AND is zero, then we know they have no common
+                // bits. We then just compare the product of the two masks' max string
+                // lengths to find a result.
                 if((maskOne & maskTwo) == 0 &&
                    maskToMaxLengthMap.get(maskOne) * maskToMaxLengthMap.get(maskTwo) > max) {
                     max = maskToMaxLengthMap.get(maskOne) * maskToMaxLengthMap.get(maskTwo);
