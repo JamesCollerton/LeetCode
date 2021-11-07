cfaa21295de8d2cafb6b080aac6a4acf19d5090a
James Collerton <jamescollerton@live.co.uk>
2021-11-07 16:31:53 +0000

    Number teams optimised

--- Number-Teams/NumberTeams.java
+++ Number-Teams/NumberTeams.java
@@ -4,13 +4,24 @@ class Solution {
         int result = 0;
         
         for(int i = 0; i < rating.length; i++) {
-            for(int j = i + 1; j < rating.length; j++) {
-                for(int k = j + 1; k < rating.length; k++) {
-                    if((rating[i] < rating[j] && rating[j] < rating[k]) || (rating[i] > rating[j] && rating[j] > rating[k])){
-                        result++; 
-                    }
+            int leftLessThan = 0, leftGreaterThan = 0;
+            int rightLessThan = 0, rightGreaterThan = 0;
+            for(int j = 0; j < i; j++) {
+                if(rating[j] < rating[i]) {
+                    leftLessThan++;
+                } else {
+                    leftGreaterThan++;
                 }
             }
+            for(int j = i + 1; j < rating.length; j++) {
+                if(rating[j] < rating[i]) {
+                    rightLessThan++;
+                } else {
+                    rightGreaterThan++;
+                }
+            }
+            
+            result += leftLessThan * rightGreaterThan + leftGreaterThan * rightLessThan;
         }
         
         return result;
