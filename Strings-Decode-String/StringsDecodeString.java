1d08de9a0a1a2abb92a4a9004a35dc9b18a3ca23
James Collerton <jamescollerton@live.co.uk>
2021-11-19 08:40:04 +0000

    Refined string

--- Strings-Decode-String/StringsDecodeString.java
+++ Strings-Decode-String/StringsDecodeString.java
@@ -2,93 +2,68 @@ class Solution {
     public String decodeString(String s) {
         
         char[] arr = s.toCharArray();
+        String result = "";
         
         Stack<Integer> numberStack = new Stack<>();
         Stack<String> stringStack = new Stack<>();
         
-        int currentNumber = 0;
-        String currentString = "";
+        int i = 0;
         
-        for(char c: arr) {
+        while(i < arr.length) {
             
-            System.out.println("");
-            System.out.println("Char: " + c);
-            System.out.println("Number stack");
-            for(int i: new ArrayList<>(numberStack)) {
-                System.out.println(i);
-            }
-            System.out.println("String stack");
-            for(String st: new ArrayList<>(stringStack)) {
-                System.out.println(st);
-            }
-            System.out.println("Current String: " + currentString);
-            System.out.println("Current Number: " + currentNumber);
-            
-            // If we're on a digit then the first time we reach
-            // a number we need to push the current string we're on
-            // onto the stack.
-            if(Character.isDigit(c)) {
+            // When we get to a character we want to start
+            // adding to it until it is finished, then push
+            // it onto the stack
+            if(Character.isDigit(arr[i])) {
                 
-                if(!"".equals(currentString)) {
-                    stringStack.push(currentString);
+                int num = 0;
+                
+                while(Character.isDigit(arr[i])) {
+                    num = num * 10 + (arr[i] - '0');
+                    i++;
                 }
                 
-                // This is in case we get subsequent numbers we need to
-                // keep it increasing
-                currentNumber = currentNumber * 10 + c - '0';
+                numberStack.push(num);
                 
-                // Reset the string for the next bracket
-                currentString = ""; 
+            // If we get to an open bracket we are starting
+            // a new string
+            } else if(arr[i] == '[') {
                 
-            // If we reached the start of a set of brackets we need to
-            // put the number on the int stack
-            } else if(c == '[') {
+                stringStack.push(result);
+                result = "";
+                i++;
                 
-                numberStack.push(currentNumber);
-                currentNumber = 0;
+            // Otherwise get whatever we have been working on,
+            // and add it x times to the last string in the
+            // stack
+            } else if(arr[i] == ']') {
                 
-            // If we reached the end of the brackets then pop the last
-            // number and string and repeat
-            } else if(c == ']') {
+                // Basically we want to get the last string
+                // from the stack and append the current string
+                // onto it x times
+                StringBuilder newResult = new StringBuilder(stringStack.pop());
+                int repeatTimes = numberStack.pop();
                 
-                if(!"".equals(currentString)) {
-                    stringStack.push(currentString);
-                    currentString = "";
+                for (int j = 0; j < repeatTimes; j++) {
+                    newResult.append(result);
                 }
                 
-                int repeatNumber = numberStack.pop();
-                String repeatString = stringStack.pop();
-                String newString = "";
+                // This is now what we're looking at, what we consider
+                // to be in the middle of the brackets
+                result = newResult.toString();
+                i++;
                 
-                for(int i = 0; i < repeatNumber; i++) {
-                    newString += repeatString;
-                }
-                
-                if(!stringStack.isEmpty()) {
-                    stringStack.push(stringStack.pop() + newString);
-                } else {
-                    stringStack.push(newString);
-                }
-                
-                // stringStack.push(stringStack.pop() + newString);
-                
-            // Here we're just adding characters to a string
             } else {
                 
-                currentString += c;
+                // If it's a character just add to the 
+                // string
+                result += arr[i];
+                i++;
                 
             }
+            
         }
         
-        stringStack.push(currentString);
-        
-        StringBuilder stringBuilder = new StringBuilder();
-        
-        while(!stringStack.isEmpty()){
-            stringBuilder.insert(0, stringStack.pop());
-        }
-        
-        return stringBuilder.toString();
-        
+        return result;
     }
 }
