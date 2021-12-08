f3467ad9f4849b7e4ebdb06b05dad6e2ff0ad202
James Collerton <jamescollerton@live.co.uk>
2021-12-08 18:17:02 +0000

    Arrays longest subarray

--- Arrays-Longest-Subarray/ArraysLongestSubarray.java
+++ Arrays-Longest-Subarray/ArraysLongestSubarray.java
@@ -5,21 +5,36 @@ class Solution {
         int p2 = 0;
         
         int maxLength = 0;
+        
         int min = nums[p1];
         int max = nums[p1];
-        
+                
         while(p2 < nums.length) {
-            
-            boolean valid = true;
-            for(int i = p1; i < p2; i++) {
-                valid = valid && (Math.abs(nums[i] - nums[p2]) <= limit);
-            }
-            
+                        
+            boolean valid = (Math.abs(min - nums[p2]) <= limit) && 
+                            (Math.abs(max - nums[p2]) <= limit);
+                        
             if(valid) {
+                
                 maxLength = Math.max(maxLength, p2 - p1 + 1);
+                
+                max = Math.max(max, nums[p2]);
+                min = Math.min(min, nums[p2]);
+                                
                 p2++;
+                
             } else {
+                
                 p1++;
+                                
+                max = nums[p1];
+                min = nums[p1];
+                
+                for(int i = p1; i < p2; i++) {
+                    max = Math.max(max, nums[i]);
+                    min = Math.min(min, nums[i]);
+                }
+                
             }
         }
         
