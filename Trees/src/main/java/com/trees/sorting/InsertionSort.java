ee8d6f574fbadff05a568f07abe5833b4b1000cb
James Collerton <jamescollerton@live.co.uk>
2021-12-31 10:06:02 +0000

    Done insertion sort

--- Trees/src/main/java/com/trees/sorting/InsertionSort.java
+++ Trees/src/main/java/com/trees/sorting/InsertionSort.java
@@ -1,4 +1,32 @@
 package com.trees.sorting;
 
 public class InsertionSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1, 3, 6, 2, 8, 9, 10, 8, 7};
+
+        int pointer = 1;
+
+        while(pointer < arr.length - 1) {
+
+            int i = pointer;
+            int pointerValue = arr[pointer];
+
+            while(i > 0 && pointerValue < arr[i - 1]) {
+                arr[i] = arr[i - 1];
+                i--;
+            }
+
+            arr[i] = pointerValue;
+
+            pointer++;
+        }
+
+        for(int i: arr) {
+            System.out.print(i + " ");
+        }
+
+    }
+
 }
