440c857fd666708388fe9389d2187d1e7611ae25
James Collerton <jamescollerton@live.co.uk>
2021-12-31 09:48:22 +0000

    Added bubble sort

--- Trees/src/main/java/com/trees/sorting/BubbleSort.java
+++ Trees/src/main/java/com/trees/sorting/BubbleSort.java
@@ -1,4 +1,30 @@
 package com.trees.sorting;
 
 public class BubbleSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1, 2, 5, 4, 3, 7, 9, 7, 10};
+
+        int pointer = 0;
+
+        while(pointer < arr.length) {
+
+            for(int i = 0; i < arr.length - 1 - pointer; i++) {
+                if(arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+
+            pointer++;
+        }
+
+        for(int i: arr) {
+            System.out.print(i + " ");
+        }
+
+    }
+
 }
