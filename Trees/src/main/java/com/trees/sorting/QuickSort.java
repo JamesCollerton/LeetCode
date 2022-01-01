7f41a8521baca1b23994d8f3ececd1364eabb53d
James Collerton <jamescollerton@live.co.uk>
2022-01-01 11:27:01 +0000

    Practicing quick sort

--- Trees/src/main/java/com/trees/sorting/QuickSort.java
+++ Trees/src/main/java/com/trees/sorting/QuickSort.java
@@ -1,4 +1,53 @@
 package com.trees.sorting;
 
+import java.util.Arrays;
+
 public class QuickSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1, 3, 6, 2, 8, 9, 10, 8, 7};
+
+        quickSort(arr, 0, arr.length - 1);
+
+        Arrays.stream(arr).forEach(System.out::println);
+
+    }
+
+    public static void quickSort(int arr[], int start, int finish) {
+
+        if(start < finish) {
+
+            int partitionIndex = partition(arr, start, finish);
+
+            quickSort(arr, start, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, finish);
+
+        }
+
+    }
+
+    private static int partition(int[] arr, int start, int finish) {
+
+        int pivot = arr[finish];
+        int i = start - 1;
+
+        for(int j = start; j < finish; j++) {
+
+            if(arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[finish];
+        arr[finish] = temp;
+
+        return i + 1;
+    }
+
 }
