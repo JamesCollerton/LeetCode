5cd9ade55e508142a731b80315ca820c7c471fdf
James Collerton <jamescollerton@live.co.uk>
2022-01-09 12:24:14 +0000

    Added path with minimum effort optimisation

--- Djikstra-Path-With-Minimum-Effort/DjikstraPathWithMinimumEffort.java
+++ Djikstra-Path-With-Minimum-Effort/DjikstraPathWithMinimumEffort.java
@@ -1,86 +1,59 @@
 class Solution {
     
-    private int numRows;
-    private int numCols;
-    private Node[][] graph;
-    private boolean[][] visited;
-    
-    class Node {
-        int distance = Integer.MAX_VALUE;
-        final int height;
-        final int row;
-        final int col;
-        Map<Node, Integer> adjacencyList = new HashMap<>();
-        
-        Node(int height, int row, int col) {
-            this.height = height;
-            this.row = row;
-            this.col = col;
-        }
-        
-        public void initialiseAdjacencyList() {
-            for(int i = -1; i <= 1; i++) {
-                for(int j = -1; j <= 1; j++) {
-                    int currRow = row + i;
-                    int currCol = col + j;
-                    if(!(currRow == row && currCol == col)) {
-                        if(currRow >= 0 && currCol >= 0 && currRow < numRows && currCol < numCols) {
-                            Node node = graph[currRow][currCol];
-                            adjacencyList.put(node, node.height);
-                        }
-                    }   
-                }
-            }
-        }
-        
-    }
+    private final int ROW = 0;
+    private final int COL = 1;
+    private int[][] moves = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     
     public int minimumEffortPath(int[][] heights) {
         
-        this.numRows = heights.length;
-        this.numCols = heights[0].length;
+        int numRows = heights.length;
+        int numCols = heights[0].length;
         
-        graph = new Node[numRows][numCols];
-        visited = new boolean[numRows][numCols];
-        
-        for(int i = 0; i < numRows; i++) {
-            for(int j = 0; j < numCols; j++) {
-                graph[i][j] = new Node(heights[i][j], i, j);
+        boolean[][] visited = new boolean[numRows][numCols];
+        int[][] distance = new int[numRows][numCols];
+                
+        for(int i = 0; i < distance.length; i++) {
+            for(int j = 0; j < distance[i].length; j++) {
+                distance[i][j] = Integer.MAX_VALUE;
             }
         }
         
-        for(int i = 0; i < numRows; i++) {
-            for(int j = 0; j < numCols; j++) {
-                graph[i][j].initialiseAdjacencyList();
-            }
-        }
-        
-        Set<Node> seen = new HashSet<>();
-        Queue<Node> queue = new PriorityQueue<>((a, b) -> a.distance - b.distance);
-        Node start = graph[0][0];
-        start.distance = 0;
+        Queue<int[]> queue = new PriorityQueue<>(
+            (a, b) -> distance[a[ROW]][a[COL]] - distance[b[ROW]][b[COL]]
+        );
+        int[] start = new int[]{0, 0};
+        distance[start[ROW]][start[COL]] = 0;
         queue.offer(start);
         
         while(!queue.isEmpty()) {
             
-            Node currentNode = queue.poll();
-            seen.add(currentNode);
-            
-            for(Map.Entry<Node, Integer> entry: currentNode.adjacencyList.entrySet()) {
-                Node node = entry.getKey();
-                int distance = entry.getValue();
-                                
-                int newDistance = Math.max(currentNode.distance, Math.abs(currentNode.height - node.height));
-                if(newDistance < node.distance) {
-                    node.distance = newDistance;
-                }
+            int[] currentCoordinates = queue.poll();
+            int currRow = currentCoordinates[ROW];
+            int currCol = currentCoordinates[COL];
+            visited[currRow][currCol] = true;
+                        
+            for(int[] move: moves) {
                 
-                if(!seen.contains(node)) {
-                    queue.offer(node);
+                int i = move[0];
+                int j = move[1];
+                int nextRow = currRow + i;
+                int nextCol = currCol + j;
+                
+                if(nextRow >= 0 && nextCol >= 0 && nextRow < numRows && nextCol < numCols) {
+
+                    int newEffort = Math.abs(heights[currRow][currCol] - heights[nextRow][nextCol]);
+                    int newDistance = Math.max(distance[currRow][currCol], newEffort);
+
+                    if(newDistance < distance[nextRow][nextCol]) {
+                        distance[nextRow][nextCol] = newDistance;
+                        queue.offer(new int[]{nextRow, nextCol});
+                    }
+
                 }
             }
+            
         }
         
-        return graph[numRows - 1][numCols - 1].distance;
+        return distance[numRows - 1][numCols - 1];
     }
 }
