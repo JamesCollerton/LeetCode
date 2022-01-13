9227299bfd72f817a5ede83c138c344c55a2b62f
James Collerton <jamescollerton@live.co.uk>
2022-01-13 20:11:51 +0000

    Skyline problem, trying different solution

--- Heaps-Skyline-Problem/HeapsSkylineProblem.java
+++ Heaps-Skyline-Problem/HeapsSkylineProblem.java
@@ -1,41 +1,83 @@
 class Solution {
     public List<List<Integer>> getSkyline(int[][] buildings) {
         
-        Map<Integer, Integer> xToMaxMap = new HashMap<>();
-        Queue<Integer> queue = new PriorityQueue<>();
-        
-        // int lastX = 0;
-        
-        for(int[] building: buildings) {
-            int left = building[0];
-            int right = building[1];
-            int height = building[2];
-            
-            // for(int i = left; i < right; i++) {
-            //     xToMaxMap.put(i, Math.max(xToMaxMap.getOrDefault(i, 0), height));
-            // }
-            xToMaxMap.put(left, Math.max(xToMaxMap.getOrDefault(left, 0), height));
-            xToMaxMap.put(right - 1, Math.max(xToMaxMap.getOrDefault(right - 1, 0), height));
-            
-            queue.offer(left);
-            queue.offer(right - 1);
-            // lastX = Math.max(lastX, right);
-        }
-        
         List<List<Integer>> result = new ArrayList<>();
-        int currentHeight = 0;
         
-        while(!queue.isEmpty()) {
-            int i = queue.poll();
-            // System.out.println(i);
-            int newHeight = xToMaxMap.getOrDefault(i, 0); 
-            if(newHeight != currentHeight) {
-                List<Integer> coordinates = List.of(i, newHeight);
-                result.add(coordinates);
-            }
-            currentHeight = newHeight;
+        // These are all the places the height can change, we have
+        // the position and the height it changes to
+        List<List<Integer>> heights = new ArrayList<>();
+        
+        // We need to separate the points out into two:
+        // - The start point, when a building height can go up
+        // - The end point, when a building height can go down
+        for(int[] b:buildings) {
+            
+            int left = b[0];
+            int right = b[1];
+            int height = b[2];
+            
+            // Start point has negative height value. Note, this
+            // negative value is used as a shortcut in the PQ and
+            // also to signify a left point.
+            heights.add(List.of(left, -height));
+            // End point has normal height value
+            heights.add(List.of(right, height)); 
         }
+
+        // Sort all the places the heights can change by
+        // position. If two places are the same then:
         
+        //  If height A negative (left point), height B positive (right point), position A (left) comes first
+        //  If height A negative (left point), height B negative (left point), greatest height comes first
+        //  If height A positive (right point), height B negative (right point), position B (right) comes first
+        //  If height A positive (right point), height B positive (right point), height with lowest height comes first
+        Collections.sort(heights, (a, b) -> {
+                int positionA = a.get(0);
+                int heightA = a.get(1);
+            
+                int positionB = b.get(0);
+                int heightB = b.get(1);
+            
+                if(positionA != positionB) {
+                    return positionA - positionB;
+                }
+                return heightA - heightB;
+        });
+
+        // Use a maxHeap to store possible heights in greatest order
+        Queue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));
+
+        // Provide a initial value to make it more consistent
+        pq.offer(0);
+
+        // Before starting, the previous max height is 0;
+        int prev = 0;
+
+        // Visit all points in order
+        for(List<Integer> height: heights) {
+            
+            int position = height.get(0);
+            int buildingHeight = height.get(1);
+            
+            // This is a start point, we can put it in
+            // the priority queue.
+            if(buildingHeight < 0) {
+                pq.offer(-buildingHeight);
+            // Otherwise this is an end point, we now
+            // need to take it off the queue as we are
+            // no longer in the remit of this building
+            } else {
+                pq.remove(buildingHeight);
+            }
+            int cur = pq.peek(); 
+
+            // compare current max height with previous max height, update result and 
+            // previous max height if necessary
+            if(prev != cur) {
+                result.add(List.of(position, cur));
+                prev = cur;
+            }
+        }
         return result;
     }
 }
