7bb9eee13f5562a238e80b7caf7b36e372c5b7be
James Collerton <jamescollerton@live.co.uk>
2022-01-15 11:52:48 +0000

    Borrowed merge sort solution

--- Linked-List-Sort/LinkedListSort.java
+++ Linked-List-Sort/LinkedListSort.java
@@ -13,66 +13,55 @@ class Solution {
         if(head == null) {
             return null;
         }
-        
-        List<ListNode> list = new ArrayList<>();
-        
-        ListNode node = head;
-        
-        while(node != null) {
-            list.add(node);
-            node = node.next;
-        }
-            
-        quickSort(list);
-        
-        for(int i = 0; i < list.size() - 1; i++) {
-            list.get(i).next = list.get(i + 1);
-        }
-        list.get(list.size() - 1).next = null;
-        
-        return list.get(0);
+                    
+        return mergeSort(head);
     }
     
-    private void quickSort(List<ListNode> list) {
-        quickSort(list, 0, list.size() - 1);
-    }
-    
-    private void quickSort(List<ListNode> list, int start, int finish) {
-        
-        if(start < finish) {
-            
-            int partitionIndex = partition(list, start, finish);
-            
-            quickSort(list, start, partitionIndex - 1);
-            quickSort(list, partitionIndex + 1, finish);
+    private ListNode mergeSort(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
         }
         
+        ListNode prev = null;
+        ListNode nodeSlow = head;
+        ListNode nodeFast = head;
+        
+        while(nodeFast != null && nodeFast.next != null) {
+            prev = nodeSlow;
+            nodeSlow = nodeSlow.next;
+            nodeFast = nodeFast.next.next;
+        }
+        
+        prev.next = null;
+        
+        ListNode leftList = mergeSort(head);
+        ListNode rightList = mergeSort(nodeSlow);
+        
+        return merge(leftList, rightList);
     }
     
-    private int partition(List<ListNode> list, int start, int finish) {
+    private ListNode merge(ListNode l1, ListNode l2) {
         
-        int pivot = list.get(finish).val;
-        int i = start - 1;
-        
-        for(int j = start; j < finish; j++) {
-            
-            if(list.get(j).val < pivot) {
-                
-                i++;
+        ListNode l = new ListNode(0), p = l;
 
-                ListNode temp = list.get(j);
-                list.set(j, list.get(i));
-                list.set(i, temp);
-                
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
             }
-            
+            p = p.next;
         }
-        
-        ListNode temp = list.get(i + 1);
-        list.set(i + 1, list.get(finish));
-        list.set(finish, temp);
 
-        return i + 1;
-        
+        if (l1 != null)
+            p.next = l1;
+
+        if (l2 != null)
+            p.next = l2;
+
+        return l.next;
     }
+    
 }
