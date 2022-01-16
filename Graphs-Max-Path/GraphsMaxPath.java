00d173ec6170f7ad80dbbd943fae4d63887786c9
James Collerton <jamescollerton@live.co.uk>
2022-01-16 10:48:46 +0000

    Done max path

--- Graphs-Max-Path/GraphsMaxPath.java
+++ Graphs-Max-Path/GraphsMaxPath.java
@@ -1,6 +1,6 @@
 class Solution {
     
-    private int max = 0;
+    private int max = 1;
     private int[][] moves = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
     private final int ROW = 0;
     private final int COL = 1;
@@ -10,56 +10,47 @@ class Solution {
         
     public int longestIncreasingPath(int[][] matrix) {
                 
+        if(matrix.length == 0) {
+            return 0;
+        }
+        
         numRows = matrix.length;
         numCols = matrix[0].length;
         this.matrix = matrix;
+        int[][] memoisation = new int[numRows][numCols];
                 
         for(int i = 0; i < numRows; i++) {
             for(int j = 0; j < numCols; j++) {
-                // System.out.println();
-                dfs(i, j, initialiseSeen(), 0);
+                max = Math.max(max, dfs(i, j, memoisation));
             }
         }
         
         return max;
     }
     
-    private void dfs(int row, int col, boolean[][] seen, int currentMax) {
+    private int dfs(int row, int col, int[][] memoisation) {
+                
+        if(memoisation[row][col] != 0) {
+            return memoisation[row][col];
+        }
         
-        seen[row][col] = true;
-        
-        max = Math.max(currentMax + 1, max);
+        int max = 1;
         
         for(int[] move: moves) {
             int newRow = row + move[ROW];
             int newCol = col + move[COL];
             
             if(newRow >= 0 && newCol >= 0 && newRow < numRows && newCol < numCols) {
-                if(!seen[newRow][newCol] && matrix[newRow][newCol] > matrix[row][col]) {
-                    dfs(newRow, newCol, cloneArray(seen), currentMax + 1);
+                if(matrix[newRow][newCol] > matrix[row][col]) {
+                    int newMax = 1 + dfs(newRow, newCol, memoisation);
+                    max = Math.max(newMax, max);
                 }
             }
         }
-                        
+        
+        memoisation[row][col] = max;
+        
+        return max;                
     }
     
-    private boolean[][] initialiseSeen() {
-        boolean[][] newArr = new boolean[numRows][numCols];
-        for(int i = 0; i < newArr.length; i++) {
-            for(int j = 0; j < newArr[0].length; j++) {
-                newArr[i][j] = false;
-            }
-        }
-        return newArr;
-    }
-    
-    private boolean[][] cloneArray(boolean[][] arr) {
-        boolean[][] newArr = new boolean[arr.length][arr[0].length];
-        for(int i = 0; i < arr.length; i++) {
-            for(int j = 0; j < arr[0].length; j++) {
-                newArr[i][j] = arr[i][j];
-            }
-        }
-        return newArr;
-    }
 }
