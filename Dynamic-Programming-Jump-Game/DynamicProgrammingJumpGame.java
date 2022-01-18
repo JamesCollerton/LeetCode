94c957ea9c5c788161071f3fb26a07d9a5508524
James Collerton <jamescollerton@live.co.uk>
2022-01-18 20:04:54 +0000

    Jump game optimised

--- Dynamic-Programming-Jump-Game/DynamicProgrammingJumpGame.java
+++ Dynamic-Programming-Jump-Game/DynamicProgrammingJumpGame.java
@@ -1,29 +1,16 @@
 class Solution {
     
-    private boolean[] seen;
-    
     public boolean canJump(int[] nums) {
-        seen = new boolean[nums.length];
-        return dfs(nums, 0);
-    }
-    
-    private boolean dfs(int[] nums, int pos) {
-        if(pos == nums.length - 1) {
-            return true;
+        
+        int max = 0;
+        int i = 0;
+        
+        while(max < nums.length && i <= max) {
+            max = Math.max(max, i + nums[i]);
+            i++;
         }
-        
-        seen[pos] = true;
-        
-        boolean result = false;
-        int steps = nums[pos];
-        
-        for(int i = 1; i <= steps; i++) {
-            if((pos + i) < seen.length && !seen[pos + i]) {
-                result = result || dfs(nums, pos + i);
-            }
-        }
-        
-        return result;
+                
+        return max >= nums.length - 1;
     }
     
 }
