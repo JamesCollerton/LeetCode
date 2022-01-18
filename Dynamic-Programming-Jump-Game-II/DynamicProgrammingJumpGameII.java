b25c85f9c291f3f752ab12c00c1c9373275ee560
James Collerton <jamescollerton@live.co.uk>
2022-01-18 20:54:21 +0000

    Jump Game II optimised

--- Dynamic-Programming-Jump-Game-II/DynamicProgrammingJumpGameII.java
+++ Dynamic-Programming-Jump-Game-II/DynamicProgrammingJumpGameII.java
@@ -2,24 +2,18 @@ class Solution {
     
     public int jump(int[] nums) {
         
-        int[] jumps = new int[nums.length];
+        int jumps = 0;
+        int currentLevelEnd = 0;
+        int max = 0;
         
-        for(int i = 0; i < jumps.length; i++) {
-            jumps[i] = Integer.MAX_VALUE;
-        }
-        
-        jumps[0] = 0;
-        
-        for(int i = 0; i < nums.length; i++) {
-            if(jumps[i] != Integer.MAX_VALUE) {
-                for(int j = 1; j <= nums[i]; j++) {
-                    if(i + j < jumps.length) {
-                        jumps[i + j] = Math.min(jumps[i + j], jumps[i] + 1);
-                    }
-                }
+        for(int i = 0; i < nums.length - 1; i++) {
+            max = Math.max(max, i + nums[i]);
+            if(i == currentLevelEnd) {
+                jumps++;
+                currentLevelEnd = max;
             }
         }
         
-        return jumps[nums.length - 1];
+        return jumps;
     }
 }
