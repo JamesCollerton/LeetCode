c27d584360abd9539f4ccc7565125b1fa7590e08
James Collerton <jamescollerton@live.co.uk>
2022-01-22 11:56:09 +0000

    Coin change accepted, optimising

--- Breadth-First-Search-Coin-Change/BreadthFirstSearchCoinChange.java
+++ Breadth-First-Search-Coin-Change/BreadthFirstSearchCoinChange.java
@@ -1,25 +1,5 @@
 class Solution {
-    
-    // private class RunningTotal {
-    //     // Do we need the coin value?
-    //     int coinValue;
-    //     int runningTotal;
-    //     int depth;
-    //     RunningTotal(int coinValue, int runningTotal, int depth) {
-    //         this.coinValue = coinValue;
-    //         this.runningTotal = runningTotal;
-    //         this.depth = depth;
-    //     }
-    //     @Override
-    //     public String toString() {
-    //         System.out.println("");
-    //         System.out.println("Coin value " + coinValue);
-    //         System.out.println("Running total " + runningTotal);
-    //         System.out.println("Depth " + depth);
-    //         return "";
-    //     }
-    // }
-    
+        
     public int coinChange(int[] coins, int amount) {
         if(amount == 0) {
             return 0;
@@ -28,41 +8,53 @@ class Solution {
         int min = Integer.MAX_VALUE;
         
         Arrays.sort(coins);
-                                
+        
+        for (int i = 0; i < coins.length / 2; i++) {
+            int temp = coins[i];
+            coins[i] = coins[coins.length - 1 - i];
+            coins[coins.length - 1 - i] = temp;
+        }
+                                        
         for(int i = 0; i < coins.length; i++) {
             
             Queue<Integer> queue = new LinkedList<>();
+            Set<Integer> seenTotals = new HashSet<>();
             queue.offer(coins[i]);
+            seenTotals.add(coins[i]);
             int depth = 1;
                                     
             while(!queue.isEmpty()) {
                 
                 int size = queue.size();
                 boolean keepGoing = true;
+                // int levelMinSize = Integer.MAX_VALUE;
                 
-                while(size > 0 && keepGoing) {
+                while(size > 0 && !queue.isEmpty() && keepGoing) {
                 
                     int currentTotal = queue.poll();
+                    // System.out.println("Current total: " + currentTotal);
 
                     for(int j = 0; j < coins.length && keepGoing; j++) {
-                        if(currentTotal > amount) {
-                            keepGoing = false;
-                        } else if(currentTotal == amount) {
+                        if(currentTotal == amount) {
                             min = Math.min(min, depth);
                             keepGoing = false;
                         } else {
                             int newRunningTotal = currentTotal + coins[j];
-                            if(newRunningTotal > 0) {
+                            if(newRunningTotal > 0 && !seenTotals.contains(newRunningTotal) && newRunningTotal <= amount) {
+                                // levelMinSize = Math.min(levelMinSize, newRunningTotal);
+                                seenTotals.add(newRunningTotal);
                                 queue.offer(newRunningTotal);
                             }
                         }
                     }
                     
+                    // System.out.println(levelMinSize);
+                    
                     size--;
                 }
                 
+                // System.out.println("Increasing depth");
                 depth++;
-                
             }
             
         }
