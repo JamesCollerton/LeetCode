8c46a15f9c1d4edfaa8ffc08a6cd94647779e2c2
James Collerton <jamescollerton@live.co.uk>
2022-03-17 12:39:44 +0000

    Optimised solution

--- Arrays-Max-Sum-Circular-Subarray/ArraysMaxSumCircularSubarray.java
+++ Arrays-Max-Sum-Circular-Subarray/ArraysMaxSumCircularSubarray.java
@@ -1,51 +1,25 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
         
-        int max = Integer.MIN_VALUE;
-        int counter = 0;
-        int pointer = 0;
-        int counterStartIndex = pointer;
+        int total = 0;
         
-        boolean keepGoing = true;
-        boolean foundNegative = false;
-        boolean secondLoop = false;
-                
-        // [1, 2, 3]
-        // [-1, -2, -3]
-        // [-2, -1, -3]
-        // [1, 2, 3, -4, 0, -5, 5]
-        // [0, 0]
-        while(keepGoing) {
+        int maxSum = nums[0];
+        int minSum = nums[0];
+        
+        int maxRunningCount = 0;
+        int minRunningCount = 0;
+        
+        for(int num: nums) {
+                        
+            maxRunningCount = Math.max(maxRunningCount + num, num);
+            maxSum = Math.max(maxSum, maxRunningCount);
             
-            int num = nums[pointer];
+            minRunningCount = Math.min(minRunningCount + num, num);
+            minSum = Math.min(minSum, minRunningCount);
             
-            if(secondLoop && (num < 0 || pointer == counterStartIndex)) {
-                keepGoing = false;
-            } else if(num < 0 || (num > 0 && counter < 0)) {
-                if(num < 0) {
-                    foundNegative = true;
-                }
-                counter = num;
-                counterStartIndex = pointer;
-            } else {
-                counter += num;
-            }
-            
-            // System.out.println("Pointer " + pointer);
-            // System.out.println("Counter " + counter);
-            
-            max = Math.max(max, counter);
-            
-            if(pointer == nums.length - 1 && foundNegative) {
-                secondLoop = true;
-                pointer = 0;
-            } else if (pointer == nums.length - 1 && !foundNegative){
-                keepGoing = false;
-            } else {
-                pointer++;
-            }
+            total += num;
         }
-        
-        return max;
+     
+        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
     }
 }
