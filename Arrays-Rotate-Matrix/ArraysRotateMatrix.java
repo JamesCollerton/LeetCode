ea45c2e75be637099b16a018d057c043c44f8864
James Collerton <jamescollerton@live.co.uk>
2022-03-29 11:11:17 +0200

    Simplified

--- Arrays-Rotate-Matrix/ArraysRotateMatrix.java
+++ Arrays-Rotate-Matrix/ArraysRotateMatrix.java
@@ -3,42 +3,20 @@ class Solution {
         
         int n = matrix.length;
         
-        int rowS = 0;
-        int rowE = n - 1;
-        int colS = 0;
-        int colE = n - 1;
-        
-        while(rowS < rowE) {
-            
-            int incr = 0;
-            int temp = 0;
-            int prevValue = 0;
-            
-            while(rowS + incr < rowE) {
-                
-                prevValue = matrix[rowS + incr][colE];
-                matrix[rowS + incr][colE] = matrix[rowS][colS + incr];
-                
-                temp = matrix[rowE][colE - incr];
-                matrix[rowE][colE - incr] = prevValue;
-                prevValue = temp;
-                
-                temp = matrix[rowE - incr][colS];
-                matrix[rowE - incr][colS] = prevValue;
-                prevValue = temp;
-                
-                matrix[rowS][colS + incr] = prevValue;
-                
-                incr++;
-            }
-            
-            rowS++;
-            rowE--;
-            colS++;
-            colE--;
-            
+        // Reverse order of rows
+        for(int i = 0; i < n / 2; i++) {
+            int[] temp = matrix[i];
+            matrix[i] = matrix[n - 1 - i]; 
+            matrix[n - 1 - i] = temp; 
         }
         
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < matrix[i].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
     }
     
 }
