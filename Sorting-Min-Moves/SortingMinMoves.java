d9606d160eb9218c0c223c5aef8199e7f0c2fa3c
James Collerton <jamescollerton@live.co.uk>
2022-03-31 12:06:49 +0200

    Another sorting problem

---
+++ Sorting-Min-Moves/SortingMinMoves.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minMoves2(int[] nums) {
+        
+        Arrays.sort(nums);
+        
+        int median = nums[nums.length / 2];
+        
+        int result = 0;
+        
+        for(int num: nums) {
+            result += Math.abs(num - median);
+        }
+        
+        return result;
+    }
+}
