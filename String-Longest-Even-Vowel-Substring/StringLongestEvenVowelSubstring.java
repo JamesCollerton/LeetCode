d43243f8d2970e5480a89a74164eec05631fbcb0
James Collerton <jamescollerton@live.co.uk>
2022-04-03 11:07:56 +0200

    Added DPg

--- String-Longest-Even-Vowel-Substring/StringLongestEvenVowelSubstring.java
+++ String-Longest-Even-Vowel-Substring/StringLongestEvenVowelSubstring.java
@@ -1,26 +1,37 @@
 class Solution {
     
-    private int max = 0;
+    private int[][] dp;
     private Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
     
     public int findTheLongestSubstring(String s) {
-                                
+                    
+        int max = 0;
+        int n = s.length();
+        dp = new int[n + 1][n + 1];
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        
         for(int i = 0; i < s.length(); i++) {
-            dfs(s, i, 0, new HashSet<>());
+            max = Math.max(max, dfs(s, i, 0, 0, new HashSet<>()));
         }
         
         return max;
     }
     
-    private void dfs(String s, int i, int count, HashSet<Character> set) {
-        if(set.isEmpty() && count > max) {
-            max = count;
+    private int dfs(String s, int i, int count, int max, HashSet<Character> set) {
+        
+        if(dp[i][count] != -1) {
+            return dp[i][count];
         }
         
         if(i >= s.length()) {
-            return;
+            return max;
         }
-        
+                        
         char c = s.charAt(i);
         if(vowels.contains(c)) {
             if(set.contains(c)) {
@@ -30,6 +41,12 @@ class Solution {
             }
         }
         
-        dfs(s, i + 1, count + 1, set);
+        int newMax = (set.isEmpty() && count + 1 > max) ? count + 1 : max;
+                
+        newMax = Math.max(newMax, dfs(s, i + 1, count + 1, newMax, set));
+        
+        dp[i][count] = newMax;
+        
+        return newMax;
     }
 }
