da7b8526862aaea1fedb3f3495f4476d19858750
James Collerton <jamescollerton@live.co.uk>
2022-04-15 09:27:07 +0200

    Sorting sort colours

--- Sort-Sort-Colours/SortSortColours.java
+++ Sort-Sort-Colours/SortSortColours.java
@@ -1,14 +1,37 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int[] colourToCount = new int[3];
-        for(int i = 0; i < nums.length; i++) {
-            colourToCount[nums[i]]++;
+
+        int startPointer = 0;
+        int currentPointer = 0;
+        
+        // 0, 1, 2, 2, 1, 0, 1, 1
+        // 0, 0, 2, 2, 1, 1, 1, 1
+        while(currentPointer < nums.length) {
+            if(nums[currentPointer] == 0) {
+                int temp = nums[startPointer];
+                nums[startPointer] = nums[currentPointer];
+                nums[currentPointer] = temp;
+                
+                startPointer++;
+                currentPointer++;
+            } else {
+                currentPointer++;
+            }
         }
-        int k = 0;
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < colourToCount[i]; j++) {
-                nums[k] = i;
-                k++;
+        
+        currentPointer = startPointer;
+        
+        // 0, 0, 2, 2, 1, 1, 1, 1
+        while(currentPointer < nums.length) {
+            if(nums[currentPointer] == 1) {
+                int temp = nums[startPointer];
+                nums[startPointer] = nums[currentPointer];
+                nums[currentPointer] = temp;
+                
+                startPointer++;
+                currentPointer++;
+            } else {
+                currentPointer++;
             }
         }
     }
