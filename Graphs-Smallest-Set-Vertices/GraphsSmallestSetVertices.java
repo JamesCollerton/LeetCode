dc1633c9939469046aa5a59c5aac7327fb58d18a
James Collerton <jamescollerton@live.co.uk>
2022-04-29 08:45:06 +0200

    Cleverer minimum vertices

--- Graphs-Smallest-Set-Vertices/GraphsSmallestSetVertices.java
+++ Graphs-Smallest-Set-Vertices/GraphsSmallestSetVertices.java
@@ -1,42 +1,17 @@
 class Solution {
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
         
-        Set<Integer> result = new HashSet<>();
-        
-        List<List<Integer>> adjacencyList = new ArrayList<>();
+        Set<Integer> startingNodes = new HashSet<>();
         
         for(int i = 0; i < n; i++) {
-            adjacencyList.add(new LinkedList<>());
+            startingNodes.add(i);
         }
         
         for(List<Integer> edge: edges) {
-            adjacencyList.get(edge.get(0)).add(edge.get(1));
+            startingNodes.remove(edge.get(1));
         }
         
-        boolean[] visited = new boolean[n];
-        
-        for(int i = 0; i < n; i++) {
-            if(!visited[i]) {
-                result.add(i);
-                dfs(visited, adjacencyList, i, result);
-            }
-        }
-        
-        return new LinkedList<>(result);
+        return new ArrayList<>(startingNodes);
     }
     
-    private void dfs(boolean[] visited, List<List<Integer>> adjacencyList, int node, Set<Integer> result) {
-        
-        visited[node] = true;
-        
-        for(int connectedNode: adjacencyList.get(node)) {
-            if(result.contains(connectedNode)) {
-                result.remove(connectedNode);
-            }
-            if(!visited[connectedNode]) {
-                dfs(visited, adjacencyList, connectedNode, result);
-            }
-        }
-        
-    }
 }
